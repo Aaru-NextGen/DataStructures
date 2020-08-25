aaa2c6a237c56ad256cf104532ccb53f0c39b1fb
AaruDevelopment <arunprabhath2010@gmail.com>
2020-08-25 12:13:27 +0530

    Add all functionality to c program for ArrayList Datatype

--- ArrayList/C/ArrayList.c
+++ ArrayList/C/ArrayList.c
@@ -1,69 +1,115 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<stdbool.h>
 
-typedef struct
-{
-    /* Create list of array based on the initial size provided or start with 2 */
+/* Create list of array based on the initial size provided or start with 2 */
+typedef struct {
     int size;
     int capacity;
     int* array;
 } ArrayList;
 
-void push(ArrayList* simpleArray, int newVal)
-{
-    int size = simpleArray->size;
-    if (size == 0){
-        simpleArray->capacity = 0;
-    }
-    int capacity = simpleArray->capacity;
 
-    if (size == capacity)
-    {
+void checkAndDoubleSize(ArrayList* simpleArray){
+    if (simpleArray->size == simpleArray->capacity){
         int* newArr;
-        if (capacity == 0 ){
+        if (simpleArray->capacity == 0 ){
             newArr = (int*)calloc(1, sizeof(int));  
             simpleArray->capacity = 1;
         } else {
-            newArr = (int*)calloc(2*(capacity), sizeof(int));  
+            newArr = (int*)calloc(2*(simpleArray->capacity), sizeof(int));  
             simpleArray->capacity *= 2;
         }
              
-        for (int i = 0; i < size; i++)
-        {
+        for (int i = 0; i < simpleArray->size; i++){
             newArr[i] = simpleArray->array[i];
         }
         free(simpleArray->array);
         simpleArray->array = newArr;
     }
-    simpleArray->array[size] = newVal;
+}
+
+void checkAndQuench(ArrayList* simpleArray){
+    if (simpleArray->size < simpleArray->capacity/4-4){
+        int* newArray;
+        newArray = (int*)calloc(simpleArray->capacity/4, sizeof(int));
+        simpleArray->capacity = simpleArray->capacity/4;
+
+        for (int i = 0; i < simpleArray->size; i++){
+         newArray[i] = simpleArray->array[i];
+        }
+
+        free(simpleArray->array);
+        simpleArray->array = newArray;
+    }
+}
+
+void push(ArrayList* simpleArray, int newVal){
+    if (simpleArray->size == 0){
+        simpleArray->capacity = 0;
+    }
+    checkAndDoubleSize(simpleArray);
+    simpleArray->array[simpleArray->size] = newVal;
     simpleArray->size += 1;
 };
 
-
-void pop(ArrayList* simpleArray, int index)
-{
-
+void pop(ArrayList* simpleArray, int index){
+    checkAndQuench(simpleArray);
+    for (int i = index; i < simpleArray->size - 1; i++){
+        simpleArray->array[i] == simpleArray->array[i+1];
+    }
+    simpleArray->size -= 1;
 };
 
-void popLast(ArrayList* simpleArray)
-{
-
+void insert(ArrayList* simpleArray, int index, int value){
+    if (index >= simpleArray->size){
+        printf("Index does not exist\n");
+        exit(1);
+    }
+    checkAndDoubleSize(simpleArray);
+    int dummyValue = simpleArray->array[index];
+    for (int i = index; i < simpleArray->size - 1; i++){
+        simpleArray->array[i] = value;
+        value = dummyValue;
+        dummyValue = simpleArray->array[i+1];
+    }
+    simpleArray->array[simpleArray->size - 1] = dummyValue;
+    simpleArray->size += 1;
 };
 
-void insert(ArrayList* simpleArray, int index, int value)
-{
+ArrayList slice(ArrayList* simpleArray, int from, int to, bool inplace){
+    if (to <= from){
+        printf("from index must be greater than to index\n");
+        exit(1);
+    }
+    if (inplace){
+        ArrayList slicedArray;
+        slicedArray.size = to - from;
+        slicedArray.capacity = (int)(2*to-from);
+        slicedArray.array = (int*)&(simpleArray->array[from]);
+        return slicedArray;
+    } else {
+        ArrayList slicedArray;
+        slicedArray.size = to - from;
+        slicedArray.capacity = (int)(2*to-from);
+        slicedArray.array = (int*)calloc(slicedArray.capacity, sizeof(int));
+        int increment = 0;
+        for (int i = from; i < to; i++){
+            slicedArray.array[increment] = simpleArray->array[i];
+            increment += 1;
+        }
+        return slicedArray;
+    }
+}
 
-};
-
-ArrayList initArrayList()
-{
+ArrayList initArrayList(){
     ArrayList array = {0};
     return array;
 };
 
 int main(){
     ArrayList simpleArray = initArrayList();
-    for (int i = 0; i< 40; i++){
+    for (int i = 0; i < 40; i++){
         push(&simpleArray, i);
         printf("capacity : %d, size : %d, value: %d\n", simpleArray.capacity, simpleArray.size, simpleArray.array[i]);
     }
--- ArrayList/ReadMe.md
+++ ArrayList/ReadMe.md
@@ -1,5 +1,13 @@
-# <ArrayList>
+**ArrayList**
 
-Functionality Requirements
+a)  ***Parameters***
+
+    1.   integer -> currentCapacity
+    2.   integer -> currentSize 
+    3.   integer pointer -> array 
+
+b) ***Functionality Requirements***
+
     1. Increase the size of an array dynamically based on the requirement
-    2. basic array functions
+    2. basic array functions list push, pop, insert, slice
+    
